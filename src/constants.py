edd0c190f9ea58b011071490f91119db24fe8255
mslilyb <lilith.r.m.d@gmail.com>
2025-02-10 16:59:54 -0800

    further progress. next step is IPC

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,18 @@
+import os
 """
-Constants for use in bakefoff script.
+Constants for use in bakefoff script. Should be in same directory as other
+parts of the source code.
 """
+def _mapsrc():
+	path = os.path.abspath(__file__)
+	path = path.split("/")[:-1]
+	path = "/".join(path) + "/src"
+	return path
 
+# Path to src directory. If I could make this private I would.
+_SRCPATH = "to be replaced :o"
+
+# List of all programs to be used in bakeoff.
 BAKEPROGS = [
 	"bbmap",
 	"blat",
@@ -19,11 +30,19 @@ BAKEPROGS = [
 	"tophat"
 ]
 
+# Allowable operating systems. Required for Conda workflow compatibility.
 OS = [
 	"Linux",
-	"iOS"
+	"Darwin"
 ]
 
+# Filename constants
 GENOMEFILE = "genome.fa"
 FTXFILE = "genome.ftx"
 READSFILE = "reads.fa.gz"
+
+# Internal source code path constants. Used for IPC between scripts.
+ALIGNER = "run-aligner.py"
+SIMULATOR = "read-simulator.py"
+
+# _mapsrc() # This is a weird idea. IDK if it's necessary.
