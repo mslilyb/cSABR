7a75875bd4bc186c7d4c062236b2fe72a31ed1f2
mslilyb <lilith.r.m.d@gmail.com>
2025-02-12 20:35:24 -0800

    fiddling more. Adding comments to better describe functions.

--- src/watchdogs.py
+++ src/watchdogs.py
@@ -17,7 +17,16 @@ To do:
 	- enable logfile control from CLI (simulprint? verbose? silent?)
 """
 
-# MESSAGES
+###########
+# LOGGING #
+###########
+
+# Initialize Logger. (Can make config file?)
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s: %(message)s', level= logging.INFO)
+
+# Messages
+
 _ERRORS = {
 	'EXISTS': "Skipping program %s. Output file already exists.",
 	'INVALID_PROG': "Skipping program %s. Program not found in bakeoff programs.",
@@ -29,23 +38,22 @@ _HELP = {
 	'MISSPELLED?': "Please double-check spelling. To add programs, please wait for me to implement that ;;"
 }
 
-# Initialize Logger. (Can make config file?)
-logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(levelname)s: %(message)s', level= logging.INFO)
-
-# FUNCTIONS
+#############
+# FUNCTIONS #
+#############
 
 # Determine if all programs exist in bakeoff pipline. Wish I could pass by
 # reference. Currently duplicate function with run-aligner.py, but not forever.
+# Returns a list of valid programs, sans the ones that do not exst.
 def verifyprograms(progs):
 	for prog in progs:
 		if prog not in bconsts.BAKEPROGS:
 			missing = progs.pop(progs.index(prog))
 			logging.error(_ERRORS['INVALID_PROG'], missing)
+			logging.info(_HELP['MISSPELLED?'])
 
 	if len(progs) < 1:
 		logging.error(_ERRORS['NO_VALID_PROGS'])
-		logging.info(_HELP['MISSPELLED?'])
 		sys.exit(1)
 
 	return progs
@@ -54,7 +62,8 @@ def verifyprograms(progs):
 
 # Determine if any files exist already and skip them. Run when option '-f' is
 # not supplied. Wish I could pass by reference. Currently duplicate function
-# with run-aligner.py, but not forever.
+# with run-aligner.py, but not forever. Returns list of valid programs, throws
+# an error and exits if none remain after checking.
 
 def noverwritten(prgs, builddir):
 	for prg in prgs:
